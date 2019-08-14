343e9346d4a22749fb9e002eb1d9ac703754df15
Calvin Shell <shell.calvin7@gmail.com>
2019-08-14 09:53:22 -0600

    build

---
+++ build/precache-manifest.e5dd8614bbc7e73f49a587f79f2a948d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee588932247c279bdc1e6281fcbe9e5",
+    "url": "/pancakes-revenge/index.html"
+  },
+  {
+    "revision": "b1805506228dd5da2cb9",
+    "url": "/pancakes-revenge/static/css/main.65e35616.chunk.css"
+  },
+  {
+    "revision": "7651922face6d9ea97c2",
+    "url": "/pancakes-revenge/static/js/2.c358ba6c.chunk.js"
+  },
+  {
+    "revision": "b1805506228dd5da2cb9",
+    "url": "/pancakes-revenge/static/js/main.50ac654b.chunk.js"
+  },
+  {
+    "revision": "cfd21dbc4e58ee13800f",
+    "url": "/pancakes-revenge/static/js/runtime~main.c4b9c7b7.js"
+  }
+]);
