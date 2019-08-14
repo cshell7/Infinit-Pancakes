2acfc571d988f65e05fa50e002d1a0a47063fc14
Calvin Shell <shell.calvin7@gmail.com>
2019-08-13 21:08:53 -0600

    build

---
+++ build/precache-manifest.ece3e54fd3b5f4285d91b095ba62d859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a79ec035075e76baedd9fbdc7b74fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7610a28abbf937c8a5",
+    "url": "/static/css/main.65e35616.chunk.css"
+  },
+  {
+    "revision": "c21298c51e05a3b31e61",
+    "url": "/static/js/2.d421d71e.chunk.js"
+  },
+  {
+    "revision": "cf7610a28abbf937c8a5",
+    "url": "/static/js/main.2e94af19.chunk.js"
+  },
+  {
+    "revision": "ae52722a02ab95154336",
+    "url": "/static/js/runtime~main.e00e3f38.js"
+  }
+]);
